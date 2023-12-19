3592e2cc442d3cbf8993249fd737cc616eb733e6
Shivam Srivastava <percyj456@gmail.com>
2023-12-20 04:17:31 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/shivam3746/chaintraced-go-backend
+
+go 1.19
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/rs/cors v1.10.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type Message struct {
+	ID     int    `json:"id"`
+	Text   string `json:"text"`
+	Author string `json:"author"`
+}
+
+var messages []Message
+
+func main() {
+	// Populating test messages
+	messages = []Message{
+		{ID: 1, Text: "Hello, World!", Author: "John Doe"},
+		{ID: 2, Text: "Testing the API", Author: "Jane Smith"},
+		{ID: 3, Text: "Golang is awesome!", Author: "Alice"},
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/messages", getMessages).Methods("GET")
+	r.HandleFunc("/messages", postMessage).Methods("POST")
+	r.HandleFunc("/messages/{id}/reply", replyToMessage).Methods("POST")
+	r.HandleFunc("/messages/{id}", deleteMessage).Methods("DELETE")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server listening on :%s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+}
+
+// Get all messages
+func getMessages(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(messages)
+}
+
+// Create a Message
+func postMessage(w http.ResponseWriter, r *http.Request) {
+	var message Message
+	if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	message.ID = len(messages) + 1
+	messages = append(messages, message)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+// Removing a message
+func deleteMessage(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for i, msg := range messages {
+		if fmt.Sprintf("%d", msg.ID) == id {
+			// Removing the message from the slice
+			messages = append(messages[:i], messages[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+// Replying to a message
+func replyToMessage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var reply Message
+	if err := json.NewDecoder(r.Body).Decode(&reply); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//Original message check
+	var originalMessage Message
+	found := false
+	for _, msg := range messages {
+		if fmt.Sprintf("%d", msg.ID) == id {
+			originalMessage = msg
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Assign a new ID and update the original message with the reply
+	reply.ID = len(messages) + 1
+	reply.Author = "Replier" // You can set the author as needed
+	messages = append(messages, reply)
+
+	// You can customize the reply logic based on your requirements
+	if originalMessage.Author == "ImportantAuthor" {
+		reply.Text = "This is an important reply!"
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(reply)
+}
